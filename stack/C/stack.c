25b6b044fa9315227cd880341142d6fc604f4954
Radek <195882@edu.p.lodz.pl>
2017-10-09 11:50:59 +0200

    Notes for refines

--- stack/C/stack.c
+++ stack/C/stack.c
@@ -3,39 +3,41 @@
 #include <stdbool.h>
 #define INTSIZE 1
 
-struct Stack{
+
+//ERROR HANDLING AND EXTENDING STACK
+typedef struct{
 	int top;
 	int *tab;
-};
+} Stack;
 
-void init(struct Stack* s){
-	s->top=0;
-	s->tab=(int*)malloc(INTSIZE*sizeof(int));
+void init(Stack* s) {
+	s->top = 0;
+	s->tab = malloc(INTSIZE*sizeof(int));
 }
-void destroy(struct Stack* s){
+void destroy(Stack* s) {
 	free(s->tab);
 }
-void push(struct Stack* s, int i){
-	if(s->top>0){
+void push(Stack* s, int i) {
+	if(s->top > 0){
 		s->tab = realloc(s->tab, (s->top+1)*sizeof(int));
 	}
-	i=s->tab[s->top];
+	i = s->tab[s->top];
 	s->top++;
 }
-int pop(struct Stack* s){
-	if(s->top==0){
-		printf("Stack is empty!");
+int pop(Stack* s) {
+	if(s->top == 0){
+		printf("Stack is empty!\n");
 		return -1;
-	}else{	
+	}else {	
 		int value;
-		value=s->top;
-		s->tab=realloc(s->tab, (s->top-1)*sizeof(int));
+		value = s->top;
+		s->tab = realloc(s->tab, (s->top-1)*sizeof(int));
 		s->top--;
 		return value;
 	}
 }
-int isEmpty(struct Stack* s){
-	if(s->top==0){
+int isEmpty(Stack* s) {
+	if(s->top == 0){
 		return true;
 	}
 	else{
@@ -43,14 +45,15 @@ int isEmpty(struct Stack* s){
 	}
 }
 
-int main(){
-	struct Stack s1, *ptr;
+int main() {
+	Stack s1, *ptr;
 	ptr=&s1;
 	init(ptr);
 	push(ptr,1);
 	push(ptr,12);
-	pop(ptr);
-	pop(ptr);
+	isEmpty(ptr);
+	//pop(ptr);
+	//pop(ptr);
 	destroy(ptr);
 	return 0;
 }
